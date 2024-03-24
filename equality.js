85935d8231a9c8d4406ed6ab7584396c297c0404
AabhashBogati <153408149+AabhashBogati@users.noreply.github.com>
2024-03-24 20:01:13 +1100

    Added equality.js

---
+++ equality.js
@@ -0,0 +1,13 @@
+//Javascript converts both values into same data type and then performs equality check
+
+if( 1 == "1"){
+    console.log("I am True")
+}
+
+//triple equals sign checks if the data type are also same and then performs equality check
+if( 2 === "2"){
+    console.log("I am True")
+}
+else{
+    console.log("I am False")
+}
